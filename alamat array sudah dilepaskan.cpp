1f644284618ef747a46db8075707bf1dd56d9781
virgygasya-lgtm <virgy.gasya@gmail.com>
2026-05-04 11:56:05 +0700

    alamat array sudah dilepaskan.cpp

---
+++ alamat array sudah dilepaskan.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+class angka
+{
+private:
+    int *arr;
+    int panjang;
+
+public:
+    angka(int); // Constructor
+    ~angka();   // Destructor
+    void cetakData();
+    void isiData();
+};
+
+angka::angka(int i){ // Constructor
+
+panjang = i;
+    arr = new int[i];
+    isiData();
+}
+
+angka::~angka(){ // Destructor
+    cout << endl;
+    cetakData();
+    delete[] arr;
+    cout << "Alamat Array Sudah Dilepaskan" << endl;
+
+}
+
+void angka::cetakData()
+{
