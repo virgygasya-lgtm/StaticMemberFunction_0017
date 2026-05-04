3c37e30b5f7a3a8a0eb3bcaaeec4b8433ca1f3c6
virgygasya-lgtm <virgy.gasya@gmail.com>
2026-05-04 11:21:09 +0700

    constructor.cpp

---
+++ constructor.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+
+class mahasiswa {
+public:
+    mahasiswa(); // Constructor
+};
+
+mahasiswa::mahasiswa() {
+    cout << "Constructor Terpanggil" << endl; // Definisi Constructor
+}
+
+int main() {
+    mahasiswa mhs; // Pembuatan Object
+    return 0;
+}
+
