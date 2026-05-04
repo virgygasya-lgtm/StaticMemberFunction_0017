39746a4c386961c718e629844e20e1ab21bbd31e
virgygasya-lgtm <virgy.gasya@gmail.com>
2026-05-04 11:29:23 +0700

    definisi construktor parameter.cpp

---
+++ definisi construktor parameter.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class mahasiswa {
+public:
+    mahasiswa(int nim, string nama); // constructor dengan parameter
+};
+
+// Definisi constructor dengan parameter
+mahasiswa::mahasiswa(int nim, string nama) {
+    cout << "Constructor Dengan Parameter Terpanggil" << endl;
+    cout << "NIM : " << nim << endl;
+    cout << "Nama : " << nama << endl;
+}
+
+
