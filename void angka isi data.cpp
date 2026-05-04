6e0a9237b7e802227872322fd30357e97b6c218b
virgygasya-lgtm <virgy.gasya@gmail.com>
2026-05-04 11:58:29 +0700

    void angka isi data.cpp

--- void angka isi data.cpp
+++ void angka isi data.cpp
@@ -31,3 +31,17 @@ angka::~angka(){ // Destructor
 
 void angka::cetakData()
 {
+for (int i = 1; i <= panjang; i++)
+    {
+        cout << i << " = " << arr[i] << endl;
+    }
+}
+
+void angka::isiData()
+{
+    for (int i = 1; i <= panjang; i++)
+    {
+        cout << i << " = ";
+        cin >> arr[i];
+    }
+    cout << endl;
