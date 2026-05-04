6473dc9cfb4d1c3917ecea96934eef831f4c40c9
virgygasya-lgtm <virgy.gasya@gmail.com>
2026-05-04 12:00:17 +0700

    constructor dan destructor dipanggil.cpp

--- constructor dan destructor dipanggil.cpp
+++ constructor dan destructor dipanggil.cpp
@@ -45,3 +45,12 @@ void angka::isiData()
         cin >> arr[i];
     }
     cout << endl;
+
+    int main()
+{
+    angka belajarcpp(3);                    // Constructor Dipanggil
+    angka *ptrBelajarcpp = new angka(5);    // Constructor Dipanggil
+    delete ptrBelajarcpp;                   // Destructor Dipanggil
+
+    return 0;
+}
